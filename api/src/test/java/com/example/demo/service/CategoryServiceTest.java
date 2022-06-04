2a7e32d66bb308fda0c917e7e64d7dd6076754d7
Pedro Duarte <pedro.dld536@gmail.com>
2022-06-04 16:42:00 +0100

    Category Service Tested And Implemented

--- api/src/test/java/com/example/demo/service/CategoryServiceTest.java
+++ api/src/test/java/com/example/demo/service/CategoryServiceTest.java
@@ -1,23 +1,22 @@
 package com.example.demo.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import com.example.demo.models.Category;
-import com.example.demo.repository.CategoryRepository;
-import com.example.demo.service.CategoryService;
-
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.internal.verification.VerificationModeFactory;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.demo.models.Category;
+import com.example.demo.repository.CategoryRepository;
 
 @ExtendWith(MockitoExtension.class)
 public class CategoryServiceTest {
@@ -28,12 +27,16 @@ public class CategoryServiceTest {
   @InjectMocks
   private CategoryService service;
 
+  private Category category1 = new Category("Fruit", true);
+  private Category category2 = new Category("Cakes", true);
+  private Category category3 = new Category("Vegetables", true);
+
   @BeforeEach
   public void setUp() {
-    Category category1 = new Category("Fruit", true);
-    Category category2 = new Category("Cakes", true);
-    Category category3 = new Category("Vegetables", true);
+    Category category4 = new Category("Fruit", true);
+    category4.setId(1L);
 
+    Mockito.when(repository.save(category1)).thenReturn(category4);
     Mockito.when(repository.findById(1L)).thenReturn(Optional.of(category1));
     Mockito.when(repository.findAll()).thenReturn(Arrays.asList(category1, category2, category3));
   }
@@ -42,12 +45,20 @@ public class CategoryServiceTest {
   public void whenGetAllCategories_thenReturnsCategoryList() {
     List<Category> categories = service.getAll();
 
-    assertEquals(3, categories.size());
+    assertThat(categories).hasSize(3);
 
-    assertEquals("Cakes", categories.get(1).getName());
-    assertEquals(true, categories.get(1).getActive());
+    assertThat(categories.get(1).getName()).isEqualTo("Cakes");
+    assertThat(categories.get(1).getActive()).isTrue();
 
     Mockito.verify(repository, VerificationModeFactory.times(1)).findAll();
+  }
+
+  @Test
+  public void whenSavingValidCategory_thenReturnsCategoryWithId() {    
+    assertThat(category1.getId()).isZero();
+    assertThat(repository.save(category1).getId()).isNotZero();
+
+    Mockito.verify(repository, VerificationModeFactory.times(1)).save(category1);
   }
 
 }
