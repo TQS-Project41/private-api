d0cf6777201c5d014b24eacf4495ea24fd0fffb0
Pedro Duarte <pedro.dld536@gmail.com>
2022-06-04 11:04:48 +0100

    Delete unnecessary hashCode and equals

--- api/src/main/java/com/example/demo/models/ListItemId.java
+++ api/src/main/java/com/example/demo/models/ListItemId.java
@@ -43,38 +43,4 @@ public class ListItemId implements Serializable {
     public void setListId(Long listId) {
         this.listId = listId;
     }
-
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((listId == null) ? 0 : listId.hashCode());
-        result = prime * result + ((productId == null) ? 0 : productId.hashCode());
-        return result;
-    }
-
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        ListItemId other = (ListItemId) obj;
-        if (listId == null) {
-            if (other.listId != null)
-                return false;
-        } else if (!listId.equals(other.listId))
-            return false;
-        if (productId == null) {
-            if (other.productId != null)
-                return false;
-        } else if (!productId.equals(other.productId))
-            return false;
-        return true;
-    }
-    
 }
