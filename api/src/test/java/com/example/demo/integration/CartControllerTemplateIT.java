470cc67a0d2ece9e6476272ba10af4629deb3cd6
Pedro Duarte <pedro.dld536@gmail.com>
2022-06-17 20:07:27 +0100

    Cart IT

--- api/src/test/java/com/example/demo/integration/CartControllerTemplateIT.java
+++ api/src/test/java/com/example/demo/integration/CartControllerTemplateIT.java
@@ -1,4 +1,5 @@
 package com.example.demo.integration;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,24 +12,23 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import com.example.demo.dto.LoginDto;
-import com.example.demo.models.Address;
+import com.example.demo.models.Category;
+import com.example.demo.models.Product;
+import com.example.demo.models.ProductListItem;
 import com.example.demo.models.User;
-import com.example.demo.models.UserAddress;
-import com.example.demo.repository.AddressRepository;
-import com.example.demo.repository.UserAddressRepository;
+import com.example.demo.repository.CartListRepository;
+import com.example.demo.repository.CategoryRepository;
+import com.example.demo.repository.ProductListItemRepository;
+import com.example.demo.repository.ProductListRepository;
+import com.example.demo.repository.ProductRepository;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.security.AuthTokenFilter;
-import com.example.demo.security.JwtUtils;
-
 
 import java.time.LocalDate;
 import java.util.HashMap;
@@ -37,8 +37,6 @@ import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyString;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
@@ -53,7 +51,6 @@ public class CartControllerTemplateIT {
         registry.add("spring.datasource.username", container::getUsername);
     }
 
-
     @LocalServerPort
     int randomServerPort;
 
@@ -64,48 +61,133 @@ public class CartControllerTemplateIT {
     private UserRepository userRepository;
 
     @Autowired
-    private AddressRepository adressRepository;
+    private CategoryRepository categoryRepository;
 
     @Autowired
-    private UserAddressRepository uaRepository;
+    private ProductRepository productRepository;
 
     @Autowired
-    private JwtUtils jwtRepo;
+    private ProductListRepository productListRepository;
 
     @Autowired
-    private SecurityContextHolder securityContextHolder;
+    private ProductListItemRepository productListItemRepository;
     @Autowired
-    private AuthTokenFilter authToken;
+    private CartListRepository cartListRepository;
+
+    @Autowired
+    private AuthTokenFilter authTokenFilter;
 
     User user;
-    Address address;    
     String token;
-    UserAddress ua;
+
+    Category categoria;
+    Product x3;
 
     @BeforeEach
-    public void setUp(){
-        Address address2 = new Address("Portugal", "1201-222", "Aveiro", "Rua das Estia");
-       
-        //String token= jwtRepo.generateJwtToken(1);
-        //LOGIN
-        User user= new User("alex20002011@gmail.com", "Alexandre", "pass",LocalDate.of(2000, 06, 28), "910123433", false, false);
+    public void setUp() {
 
-        this.user= userRepository.saveAndFlush(user);
+        // LOGIN
+        User user = new User("alex20002011@gmail.com", "Alexandre", "pass", LocalDate.of(2000, 06, 28), "910123433",
+                false, false);
+
+        this.user = userRepository.saveAndFlush(user);
         Map<String, String> request = new HashMap<>();
         request.put("email", "alex20002011@gmail.com");
         request.put("password", "pass");
-        ResponseEntity<Map> response = testRestTemplate.postForEntity("http://localhost:" + randomServerPort + "/login", request, Map.class);
+        ResponseEntity<Map> response = testRestTemplate.postForEntity("http://localhost:" + randomServerPort + "/login",
+                request, Map.class);
         this.token = response.getBody().get("token").toString();
-        //FINAL LOGIN
+        // FINAL LOGIN
+        Category categoria = new Category("FRUTA", true);
+        Product x3 = new Product("melancia", 2f, "fresca", true, categoria);
+        Product x2 = new Product("pera", 2f, "doces", true, categoria);
+        Product x = new Product("maça", 1.5f, "doces", true, categoria);
+        this.categoria = categoryRepository.saveAndFlush(categoria);
+        productRepository.saveAndFlush(x);
+        productRepository.saveAndFlush(x2);
+        this.x3 = productRepository.saveAndFlush(x3);
 
-        this.address = adressRepository.saveAndFlush(address2);
-        UserAddress ua= new UserAddress(user, address2);
-        this.ua=uaRepository.saveAndFlush(ua);
     }
 
     @AfterEach
     public void resetDb() {
-    
+        cartListRepository.deleteAll();
+        productListItemRepository.deleteAll();
+        productListRepository.deleteAll();
+        productRepository.deleteAll();
+        userRepository.deleteAll();
+        categoryRepository.deleteAll();
+
+        cartListRepository.flush();
+        productListItemRepository.flush();
+        productListRepository.flush();
+        productRepository.flush();
+        userRepository.flush();
+        categoryRepository.flush();
+
+    }
+
+    @Test
+    void testGetAddressbyInvalidUser_thenReturnNotFound() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + "");
+        HttpEntity requestEntity = new HttpEntity<Object>(headers);
+        ResponseEntity<List> response = testRestTemplate.exchange(getBaseUrl() + "/cart", HttpMethod.GET, requestEntity,
+                List.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.FORBIDDEN));
+
+    }
+
+    @Test
+    void testGetCart_thenReturnCart() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + this.token);
+        HttpEntity requestEntity = new HttpEntity<Object>(headers);
+        ResponseEntity<List> response = testRestTemplate.exchange(getBaseUrl() + "/cart", HttpMethod.GET, requestEntity,
+                List.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+
+    }
+
+    @Test
+    void testPostCart_thenReturnCart() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + this.token);
+        HttpEntity requestEntity = new HttpEntity<Object>(headers);
+        ResponseEntity<ProductListItem> response = testRestTemplate.exchange(getBaseUrl() + "/cart?product=" + x3.getId().toString() + "&amount=1",
+                HttpMethod.POST, requestEntity, ProductListItem.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.CREATED));
+
+    }
+
+    @Test
+    void testPostCartWithouArgs_thenReturnBadRequest() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + this.token);
+        HttpEntity requestEntity = new HttpEntity<Object>(headers);
+        ResponseEntity<ProductListItem> response = testRestTemplate.exchange(getBaseUrl() + "/cart/", HttpMethod.POST,
+                requestEntity, ProductListItem.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.BAD_REQUEST));
+
+    }
+
+    @Test
+    void testPostCartWithBadId_thenReturnNotFound() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + this.token);
+        HttpEntity requestEntity = new HttpEntity<Object>(headers);
+        ResponseEntity<ProductListItem> response = testRestTemplate.exchange(
+                getBaseUrl() + "/cart?product=1000&amount=1", HttpMethod.POST, requestEntity, ProductListItem.class);
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.NOT_FOUND));
+    }
+
+    private String getBaseUrl() {
+        return "http://localhost:" + randomServerPort;
     }
 
 }
